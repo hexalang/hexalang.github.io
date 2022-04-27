34c6b00de445767721f389392be36c4e9ce75a64
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2022-04-27 03:07:00 +0300

    Theme fix

--- hexalang.github.io/src/App.tsx
+++ hexalang.github.io/src/App.tsx
@@ -3,13 +3,30 @@ import { Book } from './book/Book'
 import { Try } from './try/Try'
 import { Donate } from './donate/Donate'
 import {
+	useLocation,
 	BrowserRouter,
 	Routes,
 	Route
 } from "react-router-dom"
+import { useEffect } from 'react'
+
+const ThemeFix = () => {
+	const location = useLocation()
+
+	useEffect(() => {
+		window.requestAnimationFrame(_ => {
+			const theme = `dark`
+			document.querySelectorAll(`style.${theme}rea${'der'}`).forEach((_: any) => _.media = 'max-width: 1px')
+		})
+	}, [location])
+
+	return <></>
+}
 
 const App = () => {
+
 	return <BrowserRouter>
+		<ThemeFix />
 		<Routes>
 			<Route path="/try" element={<Try />} />
 			<Route path="/donate" element={<Donate />} />
