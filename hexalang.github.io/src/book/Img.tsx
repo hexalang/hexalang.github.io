dc6e2eb2e7a09a4b622380cf51f34ea105ff2d4c
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2022-04-27 03:33:40 +0300

    Img tag features

--- hexalang.github.io/src/book/Img.tsx
+++ hexalang.github.io/src/book/Img.tsx
@@ -1,18 +1,36 @@
 import { CSSProperties, FC } from "react"
+import styled from 'styled-components'
 
 interface Props {
 	src: string
 	alt: string
 	width?: number
+	style?: CSSProperties
 }
 
 const pStyle: CSSProperties = { textAlign: "center" }
 const imgStyle: CSSProperties = { borderRadius: '4px' }
 
-export const Img: FC<Props> = ({ src, alt, width }) => {
+const StyledImg = styled.p`
+	user-select: none;
+
+	img {
+		pointer-events: none;
+	}
+
+	&& {
+		transition:	transform 0.1s ease-in-out;
+	}
+
+	&:hover:active {
+		transform: scale(0.99);
+	}
+`
+
+export const Img: FC<Props> = ({ src, alt, width, style }) => {
 	return (
-		<p style={pStyle}>
-			<img style={imgStyle} src={src} alt={alt} title={alt} width={width} height={width} />
-		</p>
+		<StyledImg style={pStyle} title={alt}>
+			<img style={{ ...imgStyle, ...style }} src={src} alt={alt} width={width} height={width} />
+		</StyledImg>
 	)
 }
