2665b95aa031b37c5e18f79db9425aff72c87833
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2022-04-28 22:05:45 +0300

    Cleanup

--- hexalang.github.io/src/book/pages/Cppnt.tsx
+++
@@ -1,5 +0,0 @@
-export const Article = () => {
-	return (
-		<>(draft)</>
-	)
-}
--- hexalang.github.io/src/data/links.tsx
+++
@@ -1,7 +0,0 @@
-export const github = 'https://github.com/hexalang'
-export const patreon = 'https://www.patreon.com/PeyTy'
-export const news = 'https://t.me/s/hexalang_news'
-export const downloads = '?'
-export const youtube = '?'
-export const telegram = 'https://t.me/s/hexalang_news/42'
-export const year = new Date().getFullYear()
