5abc856b974701a56a4cd3b61abfed79e9f3986f
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2023-04-05 01:59:20 +0300

    Update Decorators.tsx

--- hexalang.github.io/src/book/pages/lab/Decorators.tsx
+++ hexalang.github.io/src/book/pages/lab/Decorators.tsx
@@ -3,9 +3,13 @@ import { Code } from "book/Code"
 export const Article = () => {
 	return (
 		<>
-			<p>Add namespaces to <Code inline code={`@decorators`} /> and something more (soon)</p>
+			<p>Add namespaces to <Code inline code={`@decorators`} /> and something more, probably decorator aliases and fake roots.</p>
 
-			<Code code={`@namespace.decorator`} />
+			<Code code={`@namespace.decorator
+import @decorator as @alias // No 'in' clause
+@hexa.item == @item // @hexa is fake global root for built-in decorators`} />
+
+			<p>Should we add three or more namespace levels like <Code inline code={`@namespace.decorator.more.other`} /> is open for discussion.</p>
 		</>
 	)
 }
