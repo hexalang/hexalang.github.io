b8443ed92bc68978a45bd3743baebe4ddf210d0d
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2023-04-04 13:57:31 +0300

    CompilerUrl

--- hexalang.github.io/src/book/JustUrl.tsx
+++ hexalang.github.io/src/book/JustUrl.tsx
@@ -1,3 +1,26 @@
 export const JustUrl = ({ children }: { children: string }) => {
 	return <a href={children}>{children}</a>
 }
+
+const CoolUrl = styled.a`
+	&:hover * {
+		text-decoration: underline;
+	}
+
+	span {
+		color: rgb(250,96,74)
+	}
+
+	span:nth-of-type(2) {
+		color: rgb(0,174,239)
+	}
+`
+
+export const CompilerUrl = ({ children }: { children: string }) => {
+	return <CoolUrl href={children + `TODO TODO TODO`} style={{ textDecoration: 'none' }}>{
+		children
+			.replaceAll('\\', '/')
+			.split('/')
+			.map(v => <span>/{v}</span>)
+	}</CoolUrl>
+}
