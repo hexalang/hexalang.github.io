acff45560349fb7c69e58ad77a8ba732f994c683
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2023-04-05 17:12:45 +0300

    Create PSU-1.tsx

---
+++ hexalang.github.io/src/book/pages/blog/PSU-1.tsx
@@ -0,0 +1,28 @@
+import { H2 } from "book/H2"
+import { FeatureState } from "book/Markup"
+
+export const PSU1 = () => <>
+	<p>
+		Welcome to the very first issue of the Planned Syntax Updates.
+		From time-to-time we update the syntax to ensure that Hexa meets real-world requirements.
+		While we would like to never break the syntax, currently the user base is too small so the backward compatibility is not that critical.
+	</p>
+
+	<p>
+		Sometimes it becomes pretty clear that specific syntax changes feel essential.
+		It depends on the developers experience, IDE trends and other factors.
+		In Hexa, we delay features until they become a necessity.
+	</p>
+
+	<p>
+		Tell us what you think at discussions topic.
+	</p>
+
+	<H2>High priority</H2>
+	<p>
+		There is some things should be absolutely done before wide adoption:
+	</p>
+
+	<H2>More</H2>
+
+</>
