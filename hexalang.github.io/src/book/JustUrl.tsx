6b96d373a1872d3cf0e0b27eb94afa638342ace0
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2023-04-07 19:40:18 +0300

    Update JustUrl.tsx

--- hexalang.github.io/src/book/JustUrl.tsx
+++ hexalang.github.io/src/book/JustUrl.tsx
@@ -19,9 +19,10 @@ const CoolUrl = styled.a`
 `
 
 export const CompilerUrl = ({ children }: { children: string }) => {
-	return <CoolUrl href={children + `TODO TODO TODO`} style={{ textDecoration: 'none' }}>{
-		children
-			.replaceAll('\\', '/')
+	const slash = children.replaceAll('\\', '/')
+
+	return <CoolUrl href={`https://github.com/hexalang/Hexa/blob/kawaii/` + slash} style={{ textDecoration: 'none' }}>{
+		slash
 			.split('/')
 			.map(v => <span>/{v}</span>)
 	}</CoolUrl>
