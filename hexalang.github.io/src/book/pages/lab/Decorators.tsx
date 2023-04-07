11a0bf8001b29909d86672f3ebeb6112d23da537
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2023-04-07 18:43:03 +0300

    Update Decorators.tsx

--- hexalang.github.io/src/book/pages/lab/Decorators.tsx
+++ hexalang.github.io/src/book/pages/lab/Decorators.tsx
@@ -9,7 +9,11 @@ export const Article = () => {
 import @decorator as @alias // No 'in' clause
 @hexa.item == @item // @hexa is fake global root for built-in decorators`} />
 
-			<p>Should we add three or more namespace levels like <Code inline code={`@namespace.decorator.more.other`} /> is open for discussion.</p>
+			<p>
+				Should we add three or more namespace levels like <Code inline code={`@namespace.decorator.more.other`} /> is open for discussion.
+				We generally follow the rule of having only one level of namespace depth
+				(like in <Code inline code={`import * as oneLevel in ...`} /> and used as <Code inline code={`oneLevel.item`} />)
+			</p>
 		</>
 	)
 }
