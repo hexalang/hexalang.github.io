178b1933772cbba2140a4c4ab27535c293a5cd60
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2023-05-01 15:01:33 +0300

    Update Markup.tsx

--- hexalang.github.io/src/book/Markup.tsx
+++ hexalang.github.io/src/book/Markup.tsx
@@ -8,15 +8,25 @@ interface Props {
 }
 
 const StyledList = styled.ul`
+	& li::marker {
+		content: '  ';
+		font-size: 15px;
+		color: limegreen;
+		line-height: 1.0;
+	}
+
+	& li.done::marker {
+		content: '✔  ';
+	}
 `
 
 export const FeatureState: FC<Props> = ({ doc, tested, done }) => {
 	return (
 		<StyledList>
 			<br />
-			<li><b>Feature:</b>&nbsp;{done ? 'done' : 'in progress'}</li>
-			<li><b>Documentation:</b>&nbsp;{doc ? 'done' : 'in progress'}</li>
-			<li><b>Tests:</b>&nbsp;{tested ? 'done' : 'in progress'}</li>
+			<li className={done ? 'done' : ''}><b>Feature:</b>&nbsp;{done ? 'done' : 'in progress'}</li>
+			<li className={doc ? 'done' : ''}><b>Documentation:</b>&nbsp;{doc ? 'done' : 'in progress'}</li>
+			<li className={tested ? 'done' : ''}><b>Tests:</b>&nbsp;{tested ? 'done' : 'in progress'}</li>
 		</StyledList>
 	)
 }
