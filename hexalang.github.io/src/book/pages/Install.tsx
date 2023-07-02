9df22b5894395b4db80db65cf8d2df4bea54d92a
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2023-07-02 19:25:41 +0300

    Update Install.tsx

--- hexalang.github.io/src/book/pages/Install.tsx
+++ hexalang.github.io/src/book/pages/Install.tsx
@@ -1,5 +1,6 @@
 import { H2 } from "book/H2"
 import { Img } from "book/Img"
+import { JustUrl } from "book/JustUrl"
 import picture from './images/happy.png'
 
 export const Article = () => {
@@ -9,22 +10,31 @@ export const Article = () => {
 
 			<p>Getting up and running!</p>
 
-			<p>TODO</p>
-			<ul>
-				<li>Normal installers</li>
-				<li>Linux distros</li>
-				<li>IDEs</li>
-				<li>Build yourself</li>
-				<li>Nightly builds (i.e. CI Artifacts)</li>
-			</ul>
+			<p>
+				By the way, Hexa is open source!
+				If you want to build the compiler yourself please follow instructions at&nbsp;
+				<JustUrl>https://github.com/hexalang/hexa#build</JustUrl>.
+			</p>
 
 			<H2>Compiler</H2>
 
-			<p>TODO</p>
+			<p>Hexa provides graphical installers for your convenience:</p>
+
+			<ul>
+				<li>Automatic Builds <JustUrl>https://ci.appveyor.com/project/PeyTy/hexa/build/artifacts</JustUrl></li>
+				<li>GitHub Releases <JustUrl>https://github.com/hexalang/hexa/releases/latest</JustUrl></li>
+				<li>Telegram Mirror <JustUrl>https://t.me/hexalang_official</JustUrl></li>
+			</ul>
 
 			<H2>Tools</H2>
 
-			<p>TODO</p>
+			<p>Compiler includes language server and is required for tools to work properly:</p>
+
+			<ul>
+				<li>VSCode <JustUrl>https://github.com/hexalang/hexa-vscode-bundle</JustUrl></li>
+				<li>Sublime Text <JustUrl>https://github.com/hexalang/hexa-sublime-bundle</JustUrl></li>
+				<li>Atom <JustUrl>https://github.com/hexalang/hexa-atom-bundle</JustUrl></li>
+			</ul>
 		</>
 	)
 }
