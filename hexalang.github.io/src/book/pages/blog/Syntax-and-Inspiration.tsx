04ebe180bf841201242f38692bae3bddc51e50ec
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2024-09-02 16:56:29 +0300

    Update Syntax-and-Inspiration.tsx

--- hexalang.github.io/src/book/pages/blog/Syntax-and-Inspiration.tsx
+++ hexalang.github.io/src/book/pages/blog/Syntax-and-Inspiration.tsx
@@ -11,13 +11,13 @@ export const Article = () => {
 			<p>First of all, Hexa was greatly inspired by <a href="https://gist.github.com/royriojas/6454479">JavaScript</a>. It consists of things people enjoy, want, find useful and <a href="https://github.com/tc39/proposals">choose to be implemented</a>.
 				Also, JavaScript has a most flexible yet sane syntax.</p>
 
-			<p>Other guys involved are: <a href="http://www.typescriptlang.org">TypeScript</a>, <a href="http://www.apple.com/swift/">Swift</a> and, of course, <a href="http://haxe.org">Haxe</a>.</p>
+			<p>Other guys involved are: TypeScript, Swift and, of course, Haxe.</p>
 
 			<p><strong>TypeScript</strong>’s popularity shows that dynamic typed scripting fades away. I love types. It solves a JavaScript+Types problem for me.</p>
 
 			<p><strong>Swift</strong>’s team achieved high level of familiarity while rolled some pretty interesting, somewhat new, sometimes innovative, concepts and features. And yes, there is Apple’s tyranny in language space on their platforms, which makes Swift <a href="https://www.infoq.com/news/2016/01/swift-overtakes-objective-c">widely used</a> “by default”. This is my second lowest common denominator for language look and feel.</p>
 
-			<p><strong>Haxe</strong> is <em>more than 10 years old</em> language. Ahead of its time, years before it already looked like the todays mainstream. Haxe’s main powers are astonishing type system, multiplatfrom support, meta-programming and <a href="https://en.wikipedia.org/wiki/KISS_principle">KISS principle</a>. It also follows JavaScript similarity.</p>
+			<p>Haxe is <em>more than 10 years old</em> language. Ahead of its time, years before it already looked like the todays mainstream. It’s main powers are astonishing type system, multi platform support, meta-programming and <a href="https://en.wikipedia.org/wiki/KISS_principle">KISS principle</a>. It also follows JavaScript similarity.</p>
 
 			<p>You may expect a mix of features from those languages. Like <a href="https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/Functions.html">argument labels</a> at a caller side, <a href="https://blog.mariusschulz.com/2016/09/27/typescript-2-0-non-nullable-types">nullability</a> control, <a href="https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/Enumerations.html">tagged</a> unions, object <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment">extractors</a>, <a href="https://nodejs.org/api/fs.html">standard</a> library, selective data <a href="https://en.wikipedia.org/wiki/Immutable_object">immutability</a>, etcetera.</p>
 
