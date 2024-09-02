21542ad7722be2cb99970540b3542ad3c44b41fe
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2024-09-02 16:56:57 +0300

    Update Hexa-in-Hexa.tsx

--- hexalang.github.io/src/book/pages/blog/Hexa-in-Hexa.tsx
+++ hexalang.github.io/src/book/pages/blog/Hexa-in-Hexa.tsx
@@ -8,7 +8,7 @@ import { Article as Normalizer } from '../Normalizer'
 export const Article = () => {
 	return (
 		<>
-			<p>Hello! If you didn’t follow, Hexa is a static typed programming language with a touch of style, magic and beauty… oh, I mean, it compiles to JavaScript, soon <img style={{ verticalAlign: 'middle' }} src="https://haxe.org/favicon.ico" height="16" width="16" alt="Haxe" />Haxe and native. Has safety features, looks nice and simple. Coding in Hexa is really calmful and productive&nbsp;<img className="emoji" title=":cake:" alt=":cake:" src="https://github.githubassets.com/images/icons/emoji/unicode/1f370.png" height="20" width="20" /></p>
+			<p>Hello! If you didn’t follow, Hexa is a static typed programming language with a touch of style, magic and beauty… oh, I mean, it compiles to JavaScript, soon other dynamic languages and native. Has safety features, looks nice and simple. Coding in Hexa is really calmful and productive&nbsp;<img className="emoji" title=":cake:" alt=":cake:" src="https://github.githubassets.com/images/icons/emoji/unicode/1f370.png" height="20" width="20" /></p>
 
 			<p>As I wrote before in <Page page={Bootstrap}>Bootstrap the compiler</Page>, large achievement for any language is to “dogfood” a compiler with itself. Same was a goal for Hexa too (intentionally, to help with future metaprogramming features).</p>
 
