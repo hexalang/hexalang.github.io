d2ff473b2d34f030132028c301e6e2d5825d6865
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2024-09-02 05:00:59 +0300

    Update Redesign-Complex-Enums.tsx

--- hexalang.github.io/src/book/pages/lab/Redesign-Complex-Enums.tsx
+++ hexalang.github.io/src/book/pages/lab/Redesign-Complex-Enums.tsx
@@ -1,11 +1,16 @@
 import { Code } from "book/Code"
+import { H2 } from "book/H2"
 
 export const Article = () => {
 	return (
 		<>
-			<p>The idea is to make enums way more useful than they are now.</p>
+			<p>The idea is to make enums way more useful than they are now. This includes pattern matching.</p>
 
-			<p>Complex enums are enums whose tags contain extra values like <Code inline code={`TagName(value, value2)`} /> after tag definitions.</p>
+			<H2>Enumerations design</H2>
+
+			<p>
+				Complex enums are enums whose tags contain extra values like <Code inline code={`TagName(value, value2)`} /> after tag definitions.
+			</p>
 		</>
 	)
 }
