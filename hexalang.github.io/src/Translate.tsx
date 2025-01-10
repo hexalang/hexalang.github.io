89d8e886b7ce1eac03a094dedf811e41c6eda5ed
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2025-01-10 18:29:10 +0200

    Update Translate.tsx

--- hexalang.github.io/src/Translate.tsx
+++ hexalang.github.io/src/Translate.tsx
@@ -7,6 +7,19 @@ const langs: readonly string[] = (_ => {
 	// Simplify
 	let languagesIDs: readonly string[] = languages.map(_ => _.substr(0, 2).toLowerCase())
 
+	// Override with ?lang= query parameter
+	try {
+		const url = window.location
+		const params = new URLSearchParams(url.search)
+		const lang = params.get('lang')
+
+		if (lang != null && lang !== '') {
+			languagesIDs = [lang]
+		}
+	} catch (error) {
+		console.error(error)
+	}
+
 	// Fixup
 	languagesIDs = languagesIDs.map(_ => {
 		if (_ === "jp") return "ja"
@@ -18,6 +31,17 @@ const langs: readonly string[] = (_ => {
 		if (_ === "cze") return "cs"
 		if (_ === "slk") return "sk"
 		if (_ === "slo") return "sk"
+		if (_ === "gre") return "el"
+		if (_ === "per") return "fa"
+		if (_ === "mac") return "mk"
+		if (_ === "baq") return "eu"
+		if (_ === "arm") return "hy"
+		if (_ === "bur") return "my"
+		if (_ === "wel") return "cy"
+		if (_ === "ice") return "is"
+		if (_ === "geo") return "ka"
+		if (_ === "mao") return "mi"
+		// Contributor Notice: you may add more languages here
 		return _
 	})
 
